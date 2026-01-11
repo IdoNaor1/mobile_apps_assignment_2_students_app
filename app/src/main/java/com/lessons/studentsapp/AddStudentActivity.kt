7f1e907878da1a4d77f2e00dff008152ce2582d0
Tal Peled <talp2002@cs.colman.ac.il>
2026-01-11 20:07:22 +0200

    Add functionality to AddStudentActivity for saving student details

--- app/src/main/java/com/lessons/studentsapp/AddStudentActivity.kt
+++ app/src/main/java/com/lessons/studentsapp/AddStudentActivity.kt
@@ -1,10 +1,16 @@
 package com.lessons.studentsapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lessons.studentsapp.models.Model
+import com.lessons.studentsapp.models.Student
 
 class AddStudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,46 @@ class AddStudentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val nameEt = findViewById<EditText>(R.id.add_student_name)
+        val idEt = findViewById<EditText>(R.id.add_student_id)
+        val phoneEt = findViewById<EditText>(R.id.add_student_phone)
+        val addressEt = findViewById<EditText>(R.id.add_student_address)
+        val checkedCb = findViewById<CheckBox>(R.id.add_student_checked)
+
+        val cancelBtn = findViewById<Button>(R.id.add_student_cancel)
+        val saveBtn = findViewById<Button>(R.id.add_student_save)
+
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+
+        saveBtn.setOnClickListener {
+
+            val name = nameEt.text.toString().trim()
+            val id = idEt.text.toString().trim()
+            val phone = phoneEt.text.toString().trim()
+            val address = addressEt.text.toString().trim()
+            val isChecked = checkedCb.isChecked
+
+            val student = Student(
+                id = id,
+                name = name,
+                phone = phone,
+                address = address,
+                isChecked = isChecked,
+                avatarResourceId = R.drawable.student_avatar
+            )
+
+            try {
+                Model.shared.addStudent(student)
+                finish()
+            } catch (e: IllegalArgumentException) {
+                Toast.makeText(this, e.message ?: "Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
     }
 }
